fd51803c54af91867fc8bae43c6ac43ebb1fae63
Muneeb Mukhtar <muneeb.mukhtar@srijan.net>
2024-02-19 10:56:03 +0530

    Weather forcast via external API

--- weather_forecast/src/Controller/WeatherForecastController.php
+++ weather_forecast/src/Controller/WeatherForecastController.php
@@ -38,15 +38,17 @@ final class WeatherForecastController extends ControllerBase
 
     $forecaster = \Drupal::service('weather_forecast.forecast');
     $forecast = $forecaster->getByCity($city, $increments);
-    $hourly_forecast = $forecast->list;
-    $timezone = timezone_name_from_abbr('', $forecast->city->timezone, 0);
-    
+    if(!empty($forecast)){
+      $hourly_forecast = $forecast->list;
+      $timezone = timezone_name_from_abbr('', $forecast->city->timezone, 0);
+    }
+   
     // use our theme function to render twig template
     $build['content'] = [
       '#theme' => 'weather_forecast',
-      '#city' => $forecast->city->name,
-      '#timezone' => $timezone,
-      '#forecast' => $hourly_forecast,
+      '#city' => $forecast->city->name ?? '',
+      '#timezone' => $timezone ?? '',
+      '#forecast' => $hourly_forecast ?? '',
     ];
     $build['#cache']['max-age'] = 0;
     $build['content']['#attached']['library'][] = 'weather_forecast/weather-forecast';
