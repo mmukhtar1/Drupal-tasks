668d7bf49e4ed77eb4c42b57e51961706295c318
Muneeb Mukhtar <muneeb.mukhtar@srijan.net>
2024-03-06 13:42:19 +0530

    DI in passwordHash

--- user_migration_custom/src/Plugin/migrate/process/PasswordHash.php
+++ user_migration_custom/src/Plugin/migrate/process/PasswordHash.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\user_migration_custom\Plugin\migrate\process;
 
-use Drupal\migrate\ProcessPluginBase;
+use Drupal\Core\Password\PasswordInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Convert plain password to Drupal hash during user migration.
@@ -13,16 +16,80 @@ use Drupal\migrate\Row;
  *   id = "password_hash"
  * )
  */
-class PasswordHash extends ProcessPluginBase {
+class PasswordHash extends ProcessPluginBase implements ContainerFactoryPluginInterface {
 
   /**
-   * {@inheritdoc}
+   * The password hashing service.
+   *
+   * @var \Drupal\Core\Password\PasswordInterface
+   */
+  protected $passwordHasher;
+
+  /**
+   * Migration.
+   *
+   * @var \Drupal\migrate\Entity\MigrationInterface
+   */
+  protected $migration;
+
+  /**
+   * Constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Password\PasswordInterface $password_hasher
+   *   The password hasher service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, PasswordInterface $password_hasher) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->passwordHasher = $password_hasher;
+  }
+
+  /**
+   * Creates an instance of the plugin.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The container to pull out services used in the plugin.
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   *
+   * @return static
+   *   Returns an instance of this plugin.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('password')
+    );
+  }
+
+  /**
+   * Tranform will recieve the value , process it and return back.
+   *
+   * @param mixed $value
+   *   The value to be transformed.
+   * @param \Drupal\migrate\MigrateExecutableInterface $migrate_executable
+   *   The migration in which this process is being executed.
+   * @param \Drupal\migrate\Row $row
+   *   The row from the source to process.
+   * @param string $destination_property
+   *   The destination property currently worked on.
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     // Ensure the password is not empty.
     if (!empty($value)) {
       // Hash the password using Drupal's password hashing function.
-      $hashed_password = \Drupal::service('password')->hash($value);
+      $hashed_password = $this->passwordHasher->hash($value);
       return $hashed_password;
     }
     else {
